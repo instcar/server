d68e3304a1f79dae51a69353a29686d1fc4b917a
work <work@AY140222164105110546Z.(none)>
2014-04-08 22:47:11 +0800

    add room controller

--- controllers/RoomController.php
+++ controllers/RoomController.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+  *Author : Chen Haichao
+  *Time   : 2014/04/08
+  *
+  *Brief  : api for room
+  */
+
+
 namespace Instcar\Server\Controllers;
 
 class RoomController extends ControllerBase
